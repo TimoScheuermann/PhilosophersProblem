baa8ecc4e460422495f47864efe6c524dfec9898
Timo Scheuermann <timo.scheuermann@gmx.de>
2019-03-27 22:45:32 +0100

    Inital Commit
    Signed-off-by: Timo Scheuermann <timo.scheuermann@gmx.de>

---
+++ PhilosophenProblem/src/de/PhilosophenProblem/PhilosophersProblem.java
@@ -0,0 +1,17 @@
+package de.PhilosophenProblem;
+
+/**
+ * 
+ * @author Timo Scheuermann, Andrei Berbescu, Nina Wolff, Julian Homburg
+ * @since 27.03.2019
+ * @version 1.0
+ *
+ */
+
+public class PhilosophersProblem {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
